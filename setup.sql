f06d02b65d5371d31f455ea76b54f54fe499b680
Vytenis Bivainis <monrealis@gmail.com>
2020-03-10 01:06:02 +0200

    Improve books table structure

--- setup.sql
+++ setup.sql
@@ -8,21 +8,23 @@ insert into files (json)
 select pg_read_file('/docker-entrypoint-initdb.d/books.json') ::jsonb;
 
 create table books                             as
-select b->>'asin'                               asin,
-    b->>'genre'                                 genre,
-    b->>'title'                                 title,
+select substring(b->>'genre', '.*(?=:)')        genre1,
+    substring(b->>'genre', '(?<=:).*')          genre2,
     b->>'author'                                author,
-    b->>'summary'                               summary,
+    b->>'title'                                 title,
     (b->>'duration') ::interval                 duration,
-    b->>'filename'                              filename,
-    b->>'info_link'                             info_link,
-    b->>'publisher'                             publisher,
-    b->>'author_link'                           author_link,
-    b->>'narrated_by'                           narrated_by,
     (b->>'rating_count') ::int                  rating_count,
     (b->>'release_date') ::date                 release_date,
-    b->>'download_link'                         download_link,
     to_date(b->>'purchase_date', 'DD/MM/YYYY')  purchase_date,
-    (b->>'rating_average') ::numeric            rating_average
+    b->>'publisher'                             publisher,
+    b->>'genre'                                 genre,
+    b->>'filename'                              filename,
+    b->>'info_link'                             info_link,
+    b->>'author_link'                           author_link,
+    b->>'narrated_by'                           narrated_by,
+    b->>'download_link'                         download_link,
+    (b->>'rating_average') ::numeric            rating_average,
+    b->>'asin'                                  asin,
+    b->>'summary'                               summary
 from(select(jsonb_array_elements(json)) ::jsonb b
         from files) t;
